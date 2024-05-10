4a884e2383729b97d74376eefba163b065f2a336
EstherOVO <tmejwjd@gmail.com>
2024-05-10 16:29:27 +0900

    Android

---
+++ ch01_layout/src/main/java/com/busanit/ch01_layout/ex/Ex02Activity.kt
@@ -0,0 +1,16 @@
+package com.busanit.ch01_layout.ex
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.busanit.ch01_layout.R
+
+class Ex02Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_ex02)
+    }
+}
