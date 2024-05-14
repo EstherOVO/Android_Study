1a2ed0447b6a579b497f40122dd5923d690fab1b
EstherOVO <tmejwjd@gmail.com>
2024-05-14 14:52:29 +0900

    Android Resources

--- ch03_resource/src/main/java/com/busanit/ch03_resource/Resource05Activity.kt
+++ ch03_resource/src/main/java/com/busanit/ch03_resource/Resource05Activity.kt
@@ -1,6 +1,9 @@
 package com.busanit.ch03_resource
 
+import android.os.Build
 import android.os.Bundle
+import android.util.DisplayMetrics
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,5 +14,23 @@ class Resource05Activity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_resource05)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+//          API 버전 30 이상인 경우
+            val windowMetrics = windowManager.currentWindowMetrics
+            val width = windowMetrics.bounds.width()
+            val height = windowMetrics.bounds.height()
+            Log.d("myLog", "기기 너비 : ${width}, 기기 높이 : ${height}")
+
+        } else {
+//          API 버전 30보다 아래인 경우
+            val display = windowManager.defaultDisplay
+            val displayMetrics = DisplayMetrics()
+            display.getRealMetrics(displayMetrics)
+
+            val width = displayMetrics.widthPixels
+            val height = displayMetrics.heightPixels
+            Log.d("myLog", "기기 너비 : ${width}, 기기 높이 : ${height}")
+        }
     }
 }
