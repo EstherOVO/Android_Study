22a1bb50be5e991658c7e5093c890a39b1a7ebae
EstherOVO <tmejwjd@gmail.com>
2024-05-14 11:45:49 +0900

    Android Resources

--- ch03_resource/src/main/java/com/busanit/ch03_resource/Resource03Activity.kt
+++ ch03_resource/src/main/java/com/busanit/ch03_resource/Resource03Activity.kt
@@ -1,21 +1,15 @@
 package com.busanit.ch03_resource
 
 import android.os.Bundle
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.res.ResourcesCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class Resource02Activity : AppCompatActivity() {
+class Resource03Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_resource02)
-
-        val textView = findViewById<TextView>(R.id.textView3)
-
-        textView.typeface = ResourcesCompat.getFont(this, R.font.poetsen)
+        setContentView(R.layout.activity_resource03)
     }
 }
