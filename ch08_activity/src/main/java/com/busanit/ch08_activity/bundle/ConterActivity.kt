74e8796d7dd7b10e06e86034e83ad8753caa92a7
EstherOVO <tmejwjd@gmail.com>
2024-05-20 13:58:18 +0900

    Android Activity

---
+++ ch08_activity/src/main/java/com/busanit/ch08_activity/bundle/ConterActivity.kt
@@ -0,0 +1,46 @@
+package com.busanit.ch08_activity.bundle
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.busanit.ch08_activity.R
+import com.busanit.ch08_activity.databinding.ActivityConterBinding
+
+class ConterActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityConterBinding
+    var count = 0
+
+//  액티비티가 생성될 때 savedInstanceState가 매개변수로 전달됨
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityConterBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.countButton.setOnClickListener {
+            count++
+            binding.textView.text = "$count"
+        }
+    }
+
+//  액티비티가 일시적으로 소멸될 때
+//  Bundle 객체에 임시 상태 데이터를 저장할 수 있다.
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+//      번들 객체에 필요한 데이터를 저장 "Key", "Value"
+        outState.putInt("count", count)
+    }
+
+//  액티비티가 재생성 될 때 번들 객체 onRestoreInstanceState 통해 복원 가능
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val savedData: Int = savedInstanceState.getInt("count")
+        count = savedData
+        binding.textView.text = "$savedData"
+    }
+}
