24c87bb507cf5a3f6fadbffc6be8c41c849bb71c
EstherOVO <tmejwjd@gmail.com>
2024-05-21 12:31:59 +0900

    Android Recycler View

--- ch09_recycler_view/src/main/java/com/busanit/ch09_recycler_view/recyclerView/RecyclerViewActivity.kt
+++ ch09_recycler_view/src/main/java/com/busanit/ch09_recycler_view/recyclerView/RecyclerViewActivity.kt
@@ -1,4 +1,4 @@
-package com.busanit.ch09_recycler_view.list
+package com.busanit.ch09_recycler_view.recyclerView
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
@@ -7,10 +7,10 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.busanit.ch09_recycler_view.R
 
-class ListViewActivity : AppCompatActivity() {
+class RecyclerViewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_list_view)
+        setContentView(R.layout.activity_recycler_view)
     }
 }
