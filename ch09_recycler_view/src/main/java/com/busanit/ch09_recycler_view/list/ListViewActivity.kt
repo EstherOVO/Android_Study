dcf27aceb69a212c7aaa3c92cd13006318809f53
EstherOVO <tmejwjd@gmail.com>
2024-05-21 11:06:42 +0900

    Android Recycler View

---
+++ ch09_recycler_view/src/main/java/com/busanit/ch09_recycler_view/list/ListViewActivity.kt
@@ -0,0 +1,16 @@
+package com.busanit.ch09_recycler_view.list
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.busanit.ch09_recycler_view.R
+
+class ListViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_list_view)
+    }
+}
