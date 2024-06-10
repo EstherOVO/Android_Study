3e5f68e7643b2c0dc48134e5777abd61d16cfd72
EstherOVO <tmejwjd@gmail.com>
2024-06-10 16:17:23 +0900

    Android View

---
+++ ch14_etc/src/main/java/com/busanit/ch14_etc/fab/FabActivity.kt
@@ -0,0 +1,24 @@
+package com.busanit.ch14_etc.fab
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.busanit.ch14_etc.R
+import com.busanit.ch14_etc.databinding.ActivityFabBinding
+
+class FabActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityFabBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityFabBinding.inflate(layoutInflater)
+
+        setContentView(R.layout.activity_fab)
+
+        binding.fab.setOnClickListener {
+//          플로팅 액션 버튼 이벤트
+        }
+    }
+}
